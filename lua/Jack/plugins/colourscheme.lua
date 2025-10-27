190f9fd3a6f9bc126bed66eaa3000b6b934bcffe
Jack Bjerregaard <giacomo.bjerregaard@gmail.com>
2025-10-27 19:12:39 +0100

    tried a temp fix for themes, still not working fully

--- lua/Jack/plugins/colourscheme.lua
+++ lua/Jack/plugins/colourscheme.lua
@@ -4,40 +4,45 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
+      -- Custom color overrides (commented out to allow variants to work)
+      -- local bg = "#011628"
+      -- local bg_dark = "#011423"
+      -- local bg_highlight = "#143652"
+      -- local bg_search = "#0A64AC"
+      -- local bg_visual = "#275378"
+      -- local fg = "#CBE0F0"
+      -- local fg_dark = "#B4D0E9"
+      -- local fg_gutter = "#627E97"
+      -- local border = "#547998"
 
       require("tokyonight").setup({
-        style = "night",
+        -- Style is determined by colorscheme command:
+        -- :colorscheme tokyonight-night
+        -- :colorscheme tokyonight-storm
+        -- :colorscheme tokyonight-moon
+        -- :colorscheme tokyonight-day
         transparent = false, -- We'll handle transparency with autocmd
         styles = {
           sidebars = "dark",
           floats = "dark",
         },
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_float = bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = bg_dark
-          colors.bg_statusline = bg_dark
-          colors.bg_visual = bg_visual
-          colors.border = border
-          colors.fg = fg
-          colors.fg_dark = fg_dark
-          colors.fg_float = fg
-          colors.fg_gutter = fg_gutter
-          colors.fg_sidebar = fg_dark
-        end,
+        -- on_colors = function(colors)
+        --   colors.bg = bg
+        --   colors.bg_dark = bg_dark
+        --   colors.bg_float = bg_dark
+        --   colors.bg_highlight = bg_highlight
+        --   colors.bg_popup = bg_dark
+        --   colors.bg_search = bg_search
+        --   colors.bg_sidebar = bg_dark
+        --   colors.bg_statusline = bg_dark
+        --   colors.bg_visual = bg_visual
+        --   colors.border = border
+        --   colors.fg = fg
+        --   colors.fg_dark = fg_dark
+        --   colors.fg_float = fg
+        --   colors.fg_gutter = fg_gutter
+        --   colors.fg_sidebar = fg_dark
+        -- end,
       })
     end,
   },
@@ -49,11 +54,15 @@ return {
     config = function()
       require("kanagawa").setup({
         transparent = false, -- We'll handle transparency with autocmd
-        theme = "wave", -- default theme
-        background = {
-          dark = "wave",
-          light = "lotus",
-        },
+        -- Theme is determined by colorscheme command:
+        -- :colorscheme kanagawa-wave
+        -- :colorscheme kanagawa-dragon
+        -- :colorscheme kanagawa-lotus
+        -- theme = "wave", -- commented out to allow variants to work
+        -- background = {
+        --   dark = "wave",
+        --   light = "lotus",
+        -- },
       })
     end,
   },
