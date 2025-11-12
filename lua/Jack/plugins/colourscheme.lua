fdb81ade355e8621acf02f760ea54ea6e735d2a6
Jack Bjerregaard <giacomo.bjerregaard@gmail.com>
2025-11-12 13:31:55 +0100

    feat(colors): add tokyonight variant

--- lua/Jack/plugins/colourscheme.lua
+++ lua/Jack/plugins/colourscheme.lua
@@ -57,6 +57,50 @@ return {
       load_theme()
     end,
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    config = function()
+      local transparent = true
+      local colors = {
+        bg = "#011628",
+        bg_dark = "#011423",
+        bg_highlight = "#143652",
+        bg_search = "#0A64AC",
+        bg_visual = "#275378",
+        fg = "#CBE0F0",
+        fg_dark = "#B4D0E9",
+        fg_gutter = "#627E97",
+        border = "#547998",
+      }
+
+      require("tokyonight").setup({
+        style = "night",
+        transparent = transparent,
+        styles = {
+          sidebars = transparent and "transparent" or "dark",
+          floats = transparent and "transparent" or "dark",
+        },
+        on_colors = function(c)
+          c.bg = colors.bg
+          c.bg_dark = transparent and c.none or colors.bg_dark
+          c.bg_float = transparent and c.none or colors.bg_dark
+          c.bg_highlight = colors.bg_highlight
+          c.bg_popup = colors.bg_dark
+          c.bg_search = colors.bg_search
+          c.bg_sidebar = transparent and c.none or colors.bg_dark
+          c.bg_statusline = transparent and c.none or colors.bg_dark
+          c.bg_visual = colors.bg_visual
+          c.border = colors.border
+          c.fg = colors.fg
+          c.fg_dark = colors.fg_dark
+          c.fg_float = colors.fg
+          c.fg_gutter = colors.fg_gutter
+          c.fg_sidebar = colors.fg_dark
+        end,
+      })
+    end,
+  },
   { "AlexvZyl/nordic.nvim", lazy = false, opts = { transparent_bg = true } },
   { "Mofiqul/dracula.nvim", lazy = false, opts = { transparent_bg = true } },
   { "sainnhe/everforest", lazy = false },
