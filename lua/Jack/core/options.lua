360ae780e549754bebd705e02eaeec6233040f0c
Jack Bjerregaard <giacomo.bjerregaard@gmail.com>
2025-12-06 18:15:54 +0100

    idk

--- lua/Jack/core/options.lua
+++ lua/Jack/core/options.lua
@@ -32,6 +32,22 @@ opt.backspace = "indent,eol,start" --allow backspace on indent, end of line or i
 --clipboard 
 opt.clipboard:append("unnamedplus") --use system clipboard as default register 
 
+local win32yank = vim.fn.stdpath("config") .. "/bin/win32yank.exe"
+if vim.fn.executable(win32yank) == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = win32yank .. " -i --crlf",
+      ["*"] = win32yank .. " -i --crlf",
+    },
+    paste = {
+      ["+"] = win32yank .. " -o --lf",
+      ["*"] = win32yank .. " -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
+
 --split windows 
 opt.splitright = true --split vertical window to the right 
 opt.splitbelow = true --split horizontal window to the bottom 
