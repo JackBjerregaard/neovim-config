20f185a69261e9d3036f1becfedf1e5bcfe06994
Jack Bjerregaard <jack@example.com>
2026-04-26 23:09:24 +0200

    fixed f# stuff

--- lua/Jack/core/filetypes.lua
+++ lua/Jack/core/filetypes.lua
@@ -5,3 +5,13 @@ vim.filetype.add({
     fsx = "fsharp",
   },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fsharp",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
--- lua/Jack/plugins/lsp-signature.lua
+++ lua/Jack/plugins/lsp-signature.lua
@@ -2,7 +2,9 @@ return {
   "ray-x/lsp_signature.nvim",
   event = "LspAttach",
   config = function()
-    require("lsp_signature").setup({
+    local lsp_signature = require("lsp_signature")
+
+    local config = {
       bind = true,
       handler_opts = {
         border = "rounded"
@@ -15,6 +17,31 @@ return {
       transparency = 10,
       max_width = 80,
       max_height = 12,
+    }
+
+    local function attach(client, bufnr)
+      if vim.bo[bufnr].filetype == "fsharp" or (client and client.name == "fsautocomplete") then
+        return
+      end
+
+      if client and not client:supports_method("textDocument/signatureHelp") then
+        return
+      end
+
+      lsp_signature.on_attach(config, bufnr)
+    end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspSignature", {}),
+      callback = function(event)
+        attach(vim.lsp.get_client_by_id(event.data.client_id), event.buf)
+      end,
     })
+
+    for _, client in ipairs(vim.lsp.get_clients()) do
+      for bufnr in pairs(client.attached_buffers or {}) do
+        attach(client, bufnr)
+      end
+    end
   end,
 }
